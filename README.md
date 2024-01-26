3c7307d6469ac67e2cd06ffb430665c4c5b028ba
Cícero <cicerohenrique282@gmail.com>
2024-01-25 21:28:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# calculator-svelte-beginner
+
+Calculator developed in a fast and practical Svelte course at Cod3r.com.br. Svelte is a JavaScript compiler that delivers pure JavaScript or Vanilla JS, does not work with Virtual DOM, helps write less code, uses the creation of constants and variables to help you have reactive content, and has a simple project creation process. The course at Cod3r teaches Svelte in a very objective way, and the calculator application is one of the projects covered in the course.
+
+Course available in: <https://www.youtube.com/watch?v=SVNTizLyuvo>
---
+++ src/components/Botao.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+        export let texto: string
+        export let triplo: boolean = false
+        export let duplo: boolean = false
+        export let operacao: boolean = false
+        export let destaque: boolean = false
+        export let onClick: (valor: string) => void = () => {}
+</script>
+
+<button class="botao" class:triplo class:duplo class:operacao class:destaque
+        on:click={() => onClick(texto)}
+>
+        {texto}
+</button>
+
+<style>
+        .botao{
+                margin: 0%;
+                border: 1px solid var(--cor-fundo);
+                flex-basis: 25%;
+        }
+        .triplo{
+                flex-basis: 75%;
+        }
+        .duplo{
+                flex-basis: 50%;
+        }
+        .operacao{
+                background: orange;
+                color: white;
+        }
+        .destaque{
+                background: red;
+                color: white;
+        }
+</style>
